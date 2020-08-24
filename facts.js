4f969529f6c750fce507ed0b4ee9ff2a4ffc3ef3
Amir Raza <62903819+araza01@users.noreply.github.com>
2020-08-25 02:26:42 +0530

    Update facts.js

--- facts.js
+++ facts.js
@@ -18,15 +18,22 @@ function catFact(arr) {
     document.getElementById("cat-fact").innerHTML = fact;
 }
 
-
 $(document).ready(function() {
     $(document).ajaxStart(function() {
         $(".loader").show();
+        $("#cat-fact").hide();
     });
     $(document).ajaxComplete(function() {
         $(".loader").hide();
+        $("#cat-fact").show();
     });
+
     $(".btn").click(function() {
-        $("#cat-fact").load("https://cat-fact.herokuapp.com/facts/random?amount=5");
+        $.getJSON({
+            url: "https://cat-fact.herokuapp.com/facts/random?amount=5",
+            success: (data) => {
+                catFact(data);
+            } 
+        });
     });
 });
